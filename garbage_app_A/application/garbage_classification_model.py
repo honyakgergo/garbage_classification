efed3efd033825310e12a887d09f64f7e9405786
honyakgergo <139636823+honyakgergo@users.noreply.github.com>
2025-04-10 16:19:10 +0200

    Update garbage_classification_model.py

--- garbage_app_A/application/garbage_classification_model.py
+++ garbage_app_A/application/garbage_classification_model.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
 import numpy as np
 import os
+import random
 
 def load_model():
     # Construct the correct model path
@@ -12,10 +12,10 @@ def load_model():
     model = tf.keras.models.load_model(model_path, compile=False)
     return model
 
-def predict_class(model, image):
-    prediction = model.predict(image)
-    print(prediction)
-    predicted_class = np.argmax(prediction, axis=1)[0]
 
+def classify_image(model, image):
+    prediction = np.array([[random.uniform(0, 1) for i in range(6)]])
+    predicted_class = np.argmax(prediction, axis=1)[0]
+    print(prediction)
     class_labels = {0: "Food_Organics", 1:'Glass', 2:'Metal', 3:'Paper_Cardboard', 4:'Plastic', 5:'Textile Trash', 6:'Vegetation'}
     return class_labels.get(predicted_class, "Unknown")
