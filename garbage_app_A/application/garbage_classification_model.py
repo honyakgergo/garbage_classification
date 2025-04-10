374505e09321f6819d0f10b50bdb920c90e79cb7
honyakgergo <139636823+honyakgergo@users.noreply.github.com>
2025-04-10 16:37:57 +0200

    Update garbage_classification_model.py

--- garbage_app_A/application/garbage_classification_model.py
+++ garbage_app_A/application/garbage_classification_model.py
@@ -2,18 +2,7 @@ import numpy as np
 import os
 import random
 
-def load_model():
-    # Construct the correct model path
-    model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'garbage_classification_model.keras')
-
-    if not os.path.exists(model_path):
-        raise FileNotFoundError(f"Model file not found at: {model_path}")
-
-    model = tf.keras.models.load_model(model_path, compile=False)
-    return model
-
-
-def predict_class(model, image):
+def predict_class(image):
     prediction = np.array([[random.uniform(0, 1) for i in range(6)]])
     predicted_class = np.argmax(prediction, axis=1)[0]
     print(prediction)
